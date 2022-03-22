fe60ea8d02c9f3325d035235ef4eb4465941cdf7
LENOVO-M93P-BX\Lenovo <enesishala@gmail.com>
2022-03-22 10:16:42 +0100

    ﻿Route register added

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>index
+    <ul>
+        <li><h1>hello world</h1></li>
+    </ul>
+    </div>
+  )
+}
+
+export default index;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../containers/Layout";
 import Home from "../pages/home";
+import Register from "../pages/register";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Layout>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Layout>
     </BrowserRouter>
