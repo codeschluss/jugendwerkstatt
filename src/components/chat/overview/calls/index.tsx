024011cca25a0ea98ba2487582d03b26d0aa3e2a
DreniGjini <87618594+DreniGjini@users.noreply.github.com>
2022-03-28 12:31:59 +0200

    Login

---
+++ src/components/chat/overview/calls/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CallItems from "./CallItems";
+
+const Calls = () => {
+  return (
+    <div>
+      <CallItems />
+      <CallItems />
+      <CallItems />
+      <CallItems />
+      <CallItems />
+      <CallItems />
+    </div>
+  );
+};
+
+export default Calls;
