e95cb5f309e1d3f1868ed9fcc4f03750aedf40c9
DreniGjini <87618594+DreniGjini@users.noreply.github.com>
2022-04-06 22:57:45 +0200

    endpoints done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import Router from "./routes/Router";
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      alert(`GrapQlerror ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:8061/graphql" }),
+]);
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <Router />;
+  return (
+    <ApolloProvider client={client}>
+      <Router />;
+    </ApolloProvider>
+  );
 }
 
 export default App;
