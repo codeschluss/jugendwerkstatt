75f7945e21853ab25b870d7d647eaedc3ede7604
DardanB <>
2022-04-16 13:24:47 +0200

    map component

---
+++ src/components/map/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Map = () => {
+  return (
+    <div>
+      map
+    </div>
+  )
+}
+
+export default Map;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,6 +6,7 @@ import RegisteredSuccessfully from "../components/register/success/registeredSuc
 import Login from "../pages/login";
 import EmailVerification from "../pages/emailVerification";
 import EmailVerified from "../pages/emailVerified";
+import Map from '../components/map'
 
 const Router = () => {
   return (
@@ -17,6 +18,7 @@ const Router = () => {
           <Route path="/Login" element={<Login />} />
           <Route path="/EmailVerification" element={<EmailVerification />} />
           <Route path="/EmailVerified" element={<EmailVerified />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
     </BrowserRouter>
   );
