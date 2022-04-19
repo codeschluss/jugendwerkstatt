a286b3d3c1293c23ce18982248eb2855e701fd0f
DardanB <>
2022-04-19 22:36:53 +0200

    map issue fixed

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './style.css';
 
 const Map = () => {
+
   return (
     <div>
-      map
+      <MapContainer center={[40.505, -100.09]} zoom={13} >
+  
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   )
 }
