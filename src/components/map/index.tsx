055bac39505b92899ce373be4964fa96c1576740
DardanB <>
2022-04-19 22:53:56 +0200

    map events dummy

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent} from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup  } from 'react-leaflet'
 import './style.css';
 
 const Map: FunctionComponent = () => {
@@ -12,6 +12,18 @@ const Map: FunctionComponent = () => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+        <Marker position={[40.505, -100.09]}>
+            <Popup>
+              Event 1
+            </Popup>
+        </Marker>
+
+        <Marker position={[40.505, -100.12]}>
+            <Popup>
+              Event 2
+            </Popup>
+        </Marker>
       </MapContainer>
     </div>
   )
