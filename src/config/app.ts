dcded35a10f2d0db02ebf979bf84ff612f3770fe
vetemi <valmir@codeschluss.de>
2022-05-25 13:13:59 +0200

    fix typo

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,7 +3,7 @@ export const DEFAULT_DATE_FORMAT = "DD.MM.YYYY";
 export let API_URL: string;
 switch(process.env.NODE_ENV) {
   case 'production':
-    API_URL = 'https://wwww.alphaev.app/api/';
+    API_URL = 'https://www.alphaev.app/api/';
     break;
   case 'development':
   default:
