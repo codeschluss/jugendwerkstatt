b14d2c26655bf4dcaa0282596a69d4b84490fe5a
Fitim Bytyqi <fitim.bytyqi@ornio.no>
2022-06-26 23:32:00 +0200

    added error handling message for no internet connection

--- src/GraphQl/config/links/errorLink.ts
+++ src/GraphQl/config/links/errorLink.ts
@@ -1,11 +1,11 @@
-import { onError } from "@apollo/client/link/error";
-import { GQLErrorEnum } from "../../../interfaces/enums/GQLError.enum";
-import { SnackbarTypeEnum } from "../../../interfaces/enums/SnackbarType.enum";
-import { snackbarStore, expireTokenStore } from "../../../store";
+import { onError } from '@apollo/client/link/error';
+import { GQLErrorEnum } from '../../../interfaces/enums/GQLError.enum';
+import { SnackbarTypeEnum } from '../../../interfaces/enums/SnackbarType.enum';
+import { snackbarStore, expireTokenStore } from '../../../store';
 
 // Log any GraphQL errors or network error that occurred
 export const errorLink = onError(({ graphQLErrors, networkError }) => {
-  console.log("An Error Occured!", graphQLErrors, networkError);
+  console.log('An Error Occured!', graphQLErrors, networkError);
   if (graphQLErrors)
     graphQLErrors.forEach(({ extensions, message }) => {
       if (extensions.exception === GQLErrorEnum.BAD_CREDENTIALS) {
@@ -17,7 +17,7 @@ export const errorLink = onError(({ graphQLErrors, networkError }) => {
         snackbarStore.setState({
           info: {
             type: SnackbarTypeEnum.WARNING,
-            message: "Versuchen Sie es nochmal",
+            message: 'Versuchen Sie es nochmal',
           },
         });
         expireTokenStore.setState({
@@ -25,5 +25,12 @@ export const errorLink = onError(({ graphQLErrors, networkError }) => {
         });
       }
     });
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  if (networkError) {
+    snackbarStore.setState({
+      info: {
+        type: SnackbarTypeEnum.ERROR,
+        message: 'No Internet connection',
+      },
+    });
+  }
 });
