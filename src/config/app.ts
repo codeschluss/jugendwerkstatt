e2e56e4dbf021f4a08a94bebf2e05fb5a97bf5e7
vetemi <valmir@codeschluss.de>
2022-06-30 09:20:56 +0200

    fixed ws protocol

--- src/config/app.ts
+++ src/config/app.ts
@@ -11,7 +11,7 @@ switch (process.env.NODE_ENV) {
   case "development":
   default:
     BASE_URL = "https://www.alphaev.app/";
-    WS_URL = "https://www.alphaev.app/";
+    WS_URL = "ws://www.alphaev.app/";
 }
 
 API_URL = BASE_URL + "api/";
